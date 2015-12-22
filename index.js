c5ed4180bc793be4b47448580c838be8dbf2da45
Aaron Eikenberry <aeikenberry@gmail.com>
2015-12-21 23:07:22 -0600

    ugh

--- index.js
+++ index.js
@@ -13,8 +13,12 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.post('/slack/receive', function(request, response) {
+  return response.json({
+    text: "Ok"
+  });
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
