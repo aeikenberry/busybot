2d4a2f9a6063a3ff890a8b281c8beddcdf457c50
Aaron Eikenberry <aeikenberry@gmail.com>
2015-12-26 17:48:40 -0600

    body-parser

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
