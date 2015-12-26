455d1369e1cd596c35097155ba26ba492832ca14
Aaron Eikenberry <aeikenberry@gmail.com>
2015-12-26 17:12:26 -0600

    adds try

---
+++ bot.js
@@ -0,0 +1,29 @@
+var redis = require('redis');
+var client = redis.createClient(process.env.REDIS_URL);
+var scan = require('redisscan');
+
+module.exports = {
+  updateUserStatus = function(username, text) {
+    var key = 'user_status:' + username;
+    client.set(key, text);
+  },
+
+  getAllStatuses = function(callback) {
+    var returnText = 'Here\'s the latest Availability: \n\n';
+
+    scan({
+      redis: redis,
+      pattern: 'user_status:*',
+      each_callback: function (type, key, subkey, value, cb) {
+        console.log(type, key, subkey, value);
+        var username = key.split('user_status:')[1];
+        returnText += username + ': ' + value;
+        cb();
+      },
+      done_callback: function (err) {
+          redis.quit();
+          return callback(returnText);
+      }
+    });
+  }
+};
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('bodyParser');
+var redis = require('redis');
+var client = redis.createClient(process.env.REDIS_URL);
+var bot = require('./bot.js');
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -14,8 +19,25 @@ app.get('/', function(request, response) {
 });
 
 app.post('/slack/receive', function(request, response) {
-  return response.json({
-    text: "Ok"
+  /*
+  token=gIkuvaNzQIHg97ATvDxqgjtO
+  team_id=T0001
+  team_domain=example
+  channel_id=C2147483705
+  channel_name=test
+  user_id=U2147483697
+  user_name=Steve
+  command=/weather
+  text=94070
+  response_url=https://hooks.slack.com/commands/1234/5678
+  */
+
+  bot.updateUserStatus(request.body.user_name, request.body.text);
+
+  bot.getAllStatuses(function(all) {
+    return response.json({
+      text: all
+    });
   });
 });
 
